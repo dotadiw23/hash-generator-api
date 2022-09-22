1cda63857c0549f34d640d5d262480a450981d40
Juan Diego David Melo Alarcon <jdmelo02@gmail.com>
2022-09-22 18:40:49 -0500

    Se agrega la generación de hash a partir de cadenas de texto

--- controller/string_hash_controller.py
+++ controller/string_hash_controller.py
@@ -5,6 +5,7 @@ from flask_apispec import MethodResource, doc, marshal_with
 from flask_restful import Resource
 
 from dto.generar_hash_response_dto import GenerarHashResponseDto
+from hashing import algorithms
 
 
 class StringHashController(MethodResource, Resource):
@@ -13,4 +14,14 @@ class StringHashController(MethodResource, Resource):
     @marshal_with(GenerarHashResponseDto, code=HTTPStatus.OK, description='Hash generado correctamente')
     def post(self, metodo: str):
         request_payload: dict = request.get_json()
-        return make_response({'resultado': True})
+        if 'str' not in request_payload:
+            return make_response({'hash': None}, HTTPStatus.BAD_REQUEST)
+
+        if metodo == 'sha256':
+            return make_response(algorithms.generar_sha256_from_string(request_payload), HTTPStatus.CREATED)
+        elif metodo == 'sha1':
+            return make_response(algorithms.generar_sha1_from_string(request_payload), HTTPStatus.CREATED)
+        elif metodo == 'md5':
+            return make_response(algorithms.generar_md5_from_string(request_payload), HTTPStatus.CREATED)
+
+        return make_response({'hash': None}, HTTPStatus.NOT_FOUND)
